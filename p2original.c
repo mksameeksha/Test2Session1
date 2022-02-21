832f76483533469884378a648465965a0011c5b3
mksameeksha <96278091+mksameeksha@users.noreply.github.com>
2022-02-21 12:32:02 +0000

    p2

--- p2original.c
+++ p2original.c
@@ -5,9 +5,16 @@ void input_line(float *x1, float *y1,float *x2, float *y2,float *x3, float *y3)
   printf("Enter the numbers");
   scanf("%f%f%f%f%f%f",x1,y1,x2,y2,x3,y3);
 }
-int is_triangle(float x1, float y1, float x2, float y2,float x3, float y3,int *istriangle)
+int is_triangle(float x1, float y1, float x2, float y2,float x3, float y3)
 {
-  *istriangle=1/2(x1*(y2-y3)+x2*(y3-y1)+x3*(y1-y2));
+  int istriangle,a,b,c;
+  int s;
+   a=sqrt(x2-x1)+(y2-y1);
+   b=sqrt(x3-x2)+(y3-y2);
+   c=sqrt(x3-x1)+(y3-y1);
+   s=a+b+c/2;
+  istriangle=sqrt(s*(s-a)*(s-b)*(s-c));
+  return istriangle;
 }
 void output(float x1, float y1, float x2, float y2,float x3, float y3, int istriangle)
 {
@@ -23,8 +30,8 @@ void output(float x1, float y1, float x2, float y2,float x3, float y3, int istri
 int main()
 {
   float x1,y1,x2,y2,x3,y3,istriangle;
-  input(&x1,&y1,&x2,&y2,&x3,&y3);
-  is_triangle(x1,y1,x2,y2,x3,y3,&istriangle);
+  input_line(&x1,&y1,&x2,&y2,&x3,&y3);
+  istriangle=is_triangle(x1,y1,x2,y2,x3,y3);
   output(x1,y1,x2,y2,x3,y3,istriangle);
   return 0;
 }
